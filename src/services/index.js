157c49b2ddbd97e31a8c37f557b4d6ef1a69ca6a
chengmichael <chengmichael@bogon>
2025-01-12 22:20:58 +0800

    test03

--- src/services/index.js
+++ src/services/index.js
@@ -184,15 +184,15 @@ export const getStocks = async () => {
   }).catch((err) => err)
 
   if (res.status === 200 && res) {
-  const respone = res.data.data
+  const [response_1, response_2] = res.data.data
   
   const result = {
-      "510300": respone["0"].current,
+      "510300": response_1.current,
       // "510500": data[1]['current'],
       // "510300": data[0].current,
       // "510500": data[1].current,
 }
-console.log('data111', respone["0"])
+console.log('data111', response_2)
 
   // RUN_TIME_STORAGE[`Stock`] = cloneDeep(result)
 
